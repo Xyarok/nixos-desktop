c3da464fee514e8067f8f9d5c10a46bc27bb9c90
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-09-23 16:04:28 -0400

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,43 @@
-{ config, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
 
 {
-  # Import configuration files
-  imports = [
+  imports =
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./packages.nix
-      ./wm/dewm.nix
     ];
 
-  # Bootloader
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
+  # Bootloader.
+  boot = { 
+    # stay up to date on kernel
+    bootspec.enable = true;
+
+    loader = {
+      # disable boot editor
+      systemd-boot.editor = false;
+      # use systemd boot with efi
+      systemd-boot.enable = true;
+      efi = {
+        efiSysMountPoint = "/boot";
+        canTouchEfiVariables = true;
+      };
     };
   };
 
-  # Have the kernel boot 'amdgpu' right away
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  networking.hostName = "goldenTerra"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Define hostname
-  networking.hostName = "goldenTerra";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set the time zone
+  # Set your time zone.
   time.timeZone = "America/New_York";
 
   # Select internationalisation properties.
@@ -42,6 +53,21 @@
     LC_PAPER = "en_US.UTF-8";
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
+  };
+
+    # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
@@ -67,27 +93,93 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account(s)
-  users.users.rxz = {
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.xyarok = {
     isNormalUser = true;
-    description = "rxz";
+    description = "Xyarok";
     extraGroups = [ "networkmanager" "wheel" ];
+    # Personal packages
+    packages = with pkgs; [
+      
+    ];
   };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # enable flatpak
+  services.flatpak.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    # packages that are pkgs
+    gnome-connections
+    gnome-console
+    gnome-text-editor
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    # packages that are pkgs.gnome
+    epiphany
+    evince
+    geary
+    gnome-contacts
+    gnome-maps
+  ]);
+
+  # applications
+  environment.systemPackages = with pkgs; [
+    # art/photo
+    gimp
+    krita
+
+    # terminal
+    htop
+    killall
+    kitty
+    wget
+    zsh
+
+    # office/documents
+    onlyoffice-bin_latest
+    obsidian
+
+    # privacy
+    tor-browser
+
+    # video
+    lightworks
+    shotcut
+    vlc
+
+    # web
+    firefox
+    floorp
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Enable firewall
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 443 ];
-    allowedUDPPortRanges = [
-      { from = 4000; to = 4007; }
-      { from = 8000; to = 8010; }
-    ];
-  };
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-  # Set the system version.  I use unstable.
-  system.stateVersion = "23.05";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
