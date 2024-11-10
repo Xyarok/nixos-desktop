0f62486ed22087f39f89fe6f4b0e68251ccf1bc9
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-11-10 11:50:50 -0500

    Add files via upload

--- nixos/desktop/desktop.nix
+++ nixos/desktop/desktop.nix
@@ -5,21 +5,23 @@
 let
     unstable = import <unstable> {};
 in {
-  # wms
+  # wms, xwayland, +
   programs = {
     niri.enable = true;
-    waybar.enable = true;
   };
 
   # services
   services = {
     # enable gdm
-    displayManager.sddm.enable = true;
+    displayManager.ly.enable = true;
     
     # xserver related
     xserver = {
       # enable x11 windowing system.
       enable = true;
+
+      # enable gnome
+      desktopManager.gnome.enable = true;
 
 
       # enable keymap in x11
